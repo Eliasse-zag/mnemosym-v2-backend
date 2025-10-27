83bd4fc0894e4dcae895befb68ce19e27d41d945
BOUTEC Axel <axel.boutec@hotmail.fr>
2025-10-27 12:13:26 +0100

    Routes sign in up

--- models/usersShemaModel.js
+++ models/usersShemaModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
   username: String,
   password: String,
+  token: String,
 });
 
 const User = mongoose.model('users', userSchema);
