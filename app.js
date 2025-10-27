65e4323be178fc84c64e51299a75cc1370b75ecb
Eliasse <eliasse.zaghaoui@gmail.com>
2025-10-27 11:21:53 +0100

    books collection

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var booksRouter = require('./routes/books')
 
 var app = express();
 const cors = require('cors');
@@ -17,5 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.user('/books', booksRouter);
 
 module.exports = app;
