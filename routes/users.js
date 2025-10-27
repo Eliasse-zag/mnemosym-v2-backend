bd27f13b2106ebbc8123e217e4506cad37d847dd
BOUTEC Axel <axel.boutec@hotmail.fr>
2025-10-27 11:25:34 +0100

    2

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 
-/* GET users listing. */
-router.get('/users', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/signin', (req, res) => {
+  if (!checkBody(req.body, ['username', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+
+  User.findOne({ username: req.body.username }).then(data => {
+    if (data && bcrypt.compareSync(req.body.password, data.password)) {
+      res.json({ result: true, token: data.token });
+    } else {
+      res.json({ result: false, error: 'User not found or wrong password' });
+    }
+  });
 });
+
+
 
 module.exports = router;
