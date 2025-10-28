4ff7211de595d050d54ba76fb98d27a1ac21e17d
Eliasse <eliasse.zaghaoui@gmail.com>
2025-10-28 11:17:01 +0100

    route addBookByTitle

--- routes/books.js
+++ routes/books.js
@@ -25,7 +25,7 @@ router.post('/newBook/:gutendexId', async(req, res) => {
             title: bookData.title, 
             author: bookData.authors[0].name,
             synopsis: bookData.summaries[0],
-            content: textContent, // a récupérer d'un API
+            content: textContent, 
         }); 
         newBook.save().then(data => res.json({result: true, data}))
     } else { // Si le livre est dans la base de données : error
@@ -33,5 +33,56 @@ router.post('/newBook/:gutendexId', async(req, res) => {
     }
   });
 });
+
+// Ajouter un livre dans la collection Book, à partir de son titre récupéré sur le front (req.body.title)
+router.post('/addBookByTitle', async(req, res) => {
+    const title = req.body.title;
+    if (!title) {
+        return res.json({result: false, error: 'Title is required'});
+    };
+
+    // https://gutendex.com/books?search=dickens%20great
+    const response = await fetch(`https://gutendex.com/books?search=${encodeURIComponent(title)}`);
+    const searchData = await response.json();
+    //console.log("searchData", searchData) // Plusieurs oeuvres possibles, par défaut prendre le 0
+
+    if (searchData.count === 0) {
+        return res.json({result: false, error: 'No book found with this title'});
+    }
+     const frenchResult = searchData.results.find(r => r.languages?.includes('fr'));
+
+    // Vérification : il doit exister une version française
+    // Gutendex fournit un champ "languages" (ex: ['en','fr'])
+      if (frenchResult === undefined) {
+          return res.json({ result: false, error: 'No French version available for this book' });
+      } 
+
+      const bookData = frenchResult;
+      //console.log("bookData", bookData) // Première oeuvre trouvée
+      
+
+    // Récupération du texte
+    const textUrl = await fetch(`https://www.gutenberg.org/ebooks/${bookData.id}.html.images`); // récupération du text en HTML
+    const textContent = await textUrl.text(); // conversion au format text
+
+    // Trouver API avec les meta d'un livre
+    Book.findOne({gutendexId: bookData.id}).then((data) => {
+        if (data === null) { // Si le livre n'est pas dans la base de donnée : l'ajouter
+          const newBook = new Book({
+                gutendexId: bookData.id,
+                title: bookData.title, 
+                author: bookData.authors[0].name,
+                synopsis: bookData.summaries[0],
+                content: textContent
+            })
+            newBook.save().then(data => res.json({result: true, data}))
+            
+          } else { // Si le livre est dans la base de données : error
+        res.json({result: false, error: 'Book already in database'})
+    }
+})
+
+})
+
 
 module.exports = router;
